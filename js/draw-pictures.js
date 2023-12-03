384a26d4353366309fab64b723abc86088bb2ccd
AR3eS <artemfedorov04@mail.ru>
2023-12-03 21:31:10 +0500

    7.1

---
+++ js/draw-pictures.js
@@ -0,0 +1,16 @@
+export const drawMiniatures = (renderedPhotos) => {
+  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const picturesList = document.querySelector('.pictures');
+  const picturesListFragment = document.createDocumentFragment();
+
+  renderedPhotos.forEach(({url, description, likes, comments}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+    picturesListFragment.append(pictureElement);
+  });
+  picturesList.append(picturesListFragment);
+};
